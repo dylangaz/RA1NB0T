33e870248997c604d709203eb93a4f456dd59cfb
Dylan <dylangasiorowski@gmail.com>
2019-04-10 20:25:55 -0400

    v1.1.0
    Added guild add and guild remove notifications and added global command to.LowerCase()

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ bot.on('ready' , async () => {
     console.log(`Logged in as ${bot.user.username}`)
     bot.user.setStatus('available')
     bot.user.setPresence(config.defaultPresence);
+    //bot.user.setActivity(`Active in ${client.guilds.size} servers`);
 })
 commands = {};
 require(`./commands`).load(bot);
@@ -18,15 +19,24 @@ bot.on('message', (message) => {
     const prefix = "+";
     if (!message.content.startsWith(prefix)) return;
     const arguments = message.content.split(" ");
-    const command = arguments.shift().slice(prefix.length);
+    const command = arguments.shift().slice(prefix.length).toLowerCase();
     if(commands[command]){
         commands[command].execute(message, arguments);
     }
 });
+bot.on("guildDelete", guild => {
+    // this event triggers when the bot is removed from a guild.
+    console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+    //bot.user.setActivity(`Active in ${client.guilds.size} servers`);
+    bot.channels.get('565690931471187969').send(`I have been removed from: **${guild.name}** (id: ${guild.id}).`);
+  });
+bot.on("guildCreate", guild => {
+    // This event triggers when the bot joins a guild.
+    console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+    //bot.user.setActivity(`Active in ${client.guilds.size} servers`);
+    bot.channels.get('565690931471187969').send(`New guild joined: **${guild.name}** (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  });
 process.on('unhandledRejection', (r, p) => console.warn('UnhandledRejection: ', r, p))
-
-bot.login(config.token);
-
 
 //event listener to greet new guild members
 bot.on('guildMemberAdd', member => {
@@ -36,6 +46,8 @@ bot.on('guildMemberAdd', member => {
     if(!channel) return;
     channel.send(`Welcome to **__${member.guild.name}__**, ${member}`);
 });
+
+bot.login(config.token);
 
 //event listener to handle errors
 bot.on('error', console.error);
