ac46f7bd7e0c68828d7d2af2197b1a431c4c0526
Dylan <dylangasiorowski@gmail.com>
2019-07-21 21:53:31 -0400

    bug fix

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const config = require('./config.json');
-
+const prefix = config.defaultPrefix;
 bot.on('ready' , async () => {
     console.log(`Logged in as ${bot.user.username}`);
     bot.user.setStatus('available');
@@ -26,11 +26,11 @@ require(`./modules/moderation`).load(bot);
 
 bot.on('message', (message) => {
     if (message.author.id === bot.user.id) return;
+    if(message.author.bot) return;
     if(message.channel.type == "dm" && !message.content.startsWith('+')) {
         message.author.send("Hey! :wave:  If you would like to invite me to your guild, please type `+invite`, otherwise type `+help` for a list of commands!");
         return;
     }
-    const prefix = "+";
     if (!message.content.startsWith(prefix)) return;
     const arguments = message.content.split(" ");
     const command = arguments.shift().slice(prefix.length).toLowerCase();
@@ -38,22 +38,7 @@ bot.on('message', (message) => {
         commands[command].execute(message, arguments);
     }
 });
-bot.on("guildDelete", guild => {
-    // this event triggers when the bot is removed from a guild.
-
-    console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
-    bot.user.setPresence({
-        game: {
-            name: `Active in ${bot.guilds.size} servers [${config.defaultPresence.game.name}]`,
-            type: 'STREAMING',
-            url: "https://www.twitch.tv/haywirerain"
-        },
-        status: 'online'
-    });
-    bot.channels.get('565690931471187969').send(`I have been removed from: **${guild.name}** (id: ${guild.id}).`);
-  });
-bot.on("guildCreate", guild => {
-    // This event triggers when the bot joins a guild.
+bot.on("guildCreate", guild => { // This event triggers when the bot joins a guild.
 
     console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
     bot.user.setPresence({
@@ -71,6 +56,19 @@ bot.on("guildCreate", guild => {
     if(!channel) return;
     channel.send(`Thank you for inviting me to **__${guild.name}__**! \n My command prefix is '+'. \n To get started, type +help!`);
   });
+bot.on("guildDelete", guild => { // this event triggers when the bot is removed from a guild.
+
+    console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+    bot.user.setPresence({
+        game: {
+            name: `Active in ${bot.guilds.size} servers [${config.defaultPresence.game.name}]`,
+            type: 'STREAMING',
+            url: "https://www.twitch.tv/haywirerain"
+        },
+        status: 'online'
+    });
+    bot.channels.get('565690931471187969').send(`I have been removed from: **${guild.name}** (id: ${guild.id}).`);
+});
 process.on('unhandledRejection', (r, p) => console.warn('UnhandledRejection: ', r, p))
 
 //event listener to greet new guild members
